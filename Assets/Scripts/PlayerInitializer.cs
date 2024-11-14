0f7fa184b4e354d857b62397faea7237492b44bb
Ocknon <>
2024-11-14 02:45:47 -0800

    Some networking bug fixes
    Removed annoying error messages
    Fixed gravity not affecting balls when grabbed by another player while being held
    Fixed offset issue
    Fixed headset applying tracking to all players

--- Assets/Scripts/PlayerInitializer.cs
+++ Assets/Scripts/PlayerInitializer.cs
@@ -1,12 +1,16 @@
 using Normal.Realtime;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 
-public class UnownedPlayerInitializer : MonoBehaviour
+public class PlayerInitializer : MonoBehaviour
 {
     [SerializeField] private RealtimeView _realtimeView = null;
     [SerializeField] private Camera _camera = null;
     [SerializeField] private CharacterController _controller = null;
+    [SerializeField] private AudioListener _listener = null;
+    [SerializeField] private Transform _offset = null;
+    [SerializeField] private List<TrackedPoseDriver> _poseDrivers = new List<TrackedPoseDriver>();
     [SerializeField] private List<GameObject> _disabledObjects = new List<GameObject>();
 
     private bool _isOwned = false;
@@ -19,7 +23,15 @@ public class UnownedPlayerInitializer : MonoBehaviour
 
         if (_isOwned == false)
         {
+            if (_offset != null)
+            {
+                _offset.localPosition = Vector3.zero;
+            }
             DisableUnownedComponents();
+        }
+        else
+        {
+            _realtimeView.RequestOwnershipOfSelfAndChildren();
         }
     }
 
@@ -33,6 +45,16 @@ public class UnownedPlayerInitializer : MonoBehaviour
         if (_controller != null)
         {
             _controller.enabled = false;
+        }
+
+        if (_listener != null)
+        {
+            _listener.enabled = false;
+        }
+
+        foreach (var item in _poseDrivers)
+        {
+            item.enabled = false;
         }
 
         foreach (var item in _disabledObjects)
